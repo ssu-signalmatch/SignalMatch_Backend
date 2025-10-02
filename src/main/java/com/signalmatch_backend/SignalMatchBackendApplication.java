91524642858622453a471e90ac921760aae6fb8b
Seonjeongg <sj030205@naver.com>
2025-10-02 09:58:35 +0900

    초기 세팅

---
+++ src/main/java/com/signalmatch_backend/SignalMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.signalmatch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalMatchBackendApplication.class, args);
+    }
+
+}
