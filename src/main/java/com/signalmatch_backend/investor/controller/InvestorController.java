325c1368c0063bbf7ae65bd3fdd31908de537961
WINDOWLSH\ndamg <ndamgom@naver.com>
2025-10-08 08:27:41 +0900

    feat: 투자자 프로필 생성 기능 구현

--- src/main/java/com/signalmatch_backend/investor/controller/InvestorController.java
+++ src/main/java/com/signalmatch_backend/investor/controller/InvestorController.java
@@ -25,7 +25,7 @@ public class InvestorController {
 
     @PostMapping("/investors/me")
     @Operation(summary = "투자자 프로필 생성", description = "로그인한 사용자의 투자자 프로필을 생성합니다.")
-    public ResponseEntity<ApiResponse> investorProfileCreate(@AuthenticationPrincipal CustomUserDetails customUserDetails, @Valid @RequestBody InvestorProfileCreateRequest request){
+    public ResponseEntity<ApiResponse<InvestorProfileCreateResponse>> investorProfileCreate(@AuthenticationPrincipal CustomUserDetails customUserDetails, @Valid @RequestBody InvestorProfileCreateRequest request){
         Long userId = customUserDetails.getUser().getUserId();
         InvestorProfileCreateResponse response = investorService.createInvestorProfile(
             userId, request);
