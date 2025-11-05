36ce2339707fb76373c0d54dd435f62c00978fc3
LeeSeohyeon111 <osey7y7@naver.com>
2025-11-05 16:57:17 +0900

    feat: 즐겨찾기 목록 조회

---
+++ src/main/java/com/signalmatch_backend/bookmark/dto/BookmarkListResponse.java
@@ -0,0 +1,15 @@
+package com.signalmatch_backend.bookmark.dto;
+
+import com.signalmatch_backend.bookmark.domain.Bookmark;
+import com.signalmatch_backend.investor.domain.Investor;
+import com.signalmatch_backend.startup.domain.Startup;
+import lombok.Builder;
+
+@Builder
+public record BookmarkListResponse(
+    String targetType,
+    Long targetId,
+    String name
+
+) {
+}
--- src/main/java/com/signalmatch_backend/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/signalmatch_backend/bookmark/repository/BookmarkRepository.java
@@ -1,6 +1,7 @@
 package com.signalmatch_backend.bookmark.repository;
 
 import com.signalmatch_backend.bookmark.domain.Bookmark;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
     Optional<Bookmark> findByUserIdAndInvestorId(Long userId, Long investorId);
 
     Optional<Bookmark> findByUserIdAndStartupId(Long userId, Long startupId);
+
+    List<Bookmark> findByUserId(Long userId);
 }
